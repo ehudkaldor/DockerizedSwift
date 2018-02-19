cccfaadae009e2e281fed19f0b15c1a9527c8ff9
Ehud Kaldor <ehud@unfairfunction.org>
2018-02-19 14:07:10 -0800

    changed from pip/setup.py to easy_install

--- dockerfile_install_scripts/swift_install.sh
+++ dockerfile_install_scripts/swift_install.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
 cd /opt
-  git clone https://github.com/openstack/swift.git
-  cd /opt/swift
-  # export PYTHONLIB=/usr/local/lib && \
-  python setup.py install
-  # pip3 install -e . && \
+git clone https://github.com/openstack/swift.git
+cd /opt/swift
+easy_install-2.7 .
 
-  cp /opt/swift/doc/saio/bin/* $HOME/bin
-  chmod +x $HOME/bin/*
-  sed -i "s/bash/sh/g" $HOME/bin/*
-  echo "export PATH=${PATH}:$HOME/bin" >> $HOME/.bashrc
+# export PYTHONLIB=/usr/local/lib && \
+# python setup.py install
+# pip3 install -e . && \
 
-  cd /opt
-  rm -rf swift
+
+cp /opt/swift/doc/saio/bin/* $HOME/bin
+chmod +x $HOME/bin/*
+sed -i "s/bash/sh/g" $HOME/bin/*
+sed -i "s/sudo //g" $HOME/bin/*
+mkdir /root/tmp
+echo "export PATH=${PATH}:$HOME/bin" >> $HOME/.shrc
+echo "export PYTHON_EGG_CACHE=/root/tmp" >> $HOME/.shrc
+echo "export ENV=$HOME/.shrc" >> $HOME/.profile
+chmod +x $HOME/.shrc
+chmod +x $HOME/.profile
+
+cd /opt
+rm -rf swift
