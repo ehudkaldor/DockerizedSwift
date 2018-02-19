8d6de90cb4f674ffd848baa83581866d0234df7c
Ehud Kaldor <ehud@unfairfunction.org>
2018-02-19 14:07:22 -0800

    cosmetic

--- dockerfile_install_scripts/xattr_install.sh
+++ dockerfile_install_scripts/xattr_install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 cd /opt
-  git clone https://github.com/xattr/xattr.git
-  cd xattr
-  sed -i "s/u_int32_t/uint32_t/g" xattr/lib_build.py
-  # python3 setup.py install
-  pip install -e .
-  cd /opt 
-  rm -rf xattr
+git clone https://github.com/xattr/xattr.git
+cd xattr
+sed -i "s/u_int32_t/uint32_t/g" xattr/lib_build.py
+# python3 setup.py install
+# pip install -e .
+easy_install .
+cd /opt
+rm -rf xattr
