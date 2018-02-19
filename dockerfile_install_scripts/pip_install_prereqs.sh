12dba453521cb15cdd1ce455181a5b8ef9cd82b6
Ehud Kaldor <ehud@unfairfunction.org>
2018-02-19 14:05:55 -0800

    added upgrade

--- dockerfile_install_scripts/pip_install_prereqs.sh
+++ dockerfile_install_scripts/pip_install_prereqs.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
 echo "+ + + + + + + + + + installing pastedeploy"
-pip install pastedeploy
+pip install --upgrade pastedeploy
 echo "+ + + + + + + + + + installing eventlet"
-pip install eventlet
+pip install --upgrade eventlet
 echo "+ + + + + + + + + + installing greenlet"
-pip install greenlet
+pip install --upgrade greenlet
 echo "+ + + + + + + + + + installing netifaces"
-pip install netifaces
+pip install --upgrade netifaces
 echo "+ + + + + + + + + + installing simplejson"
-pip install simplejson
+pip install --upgrade simplejson
 echo "+ + + + + + + + + + installing setuptools"
-pip install setuptools
+pip install --upgrade setuptools
+# echo "+ + + + + + + + + + installing distribute"
+# pip install --upgrade distribute
 echo "+ + + + + + + + + + installing six"
-pip install six
+pip install --upgrade six
 echo "+ + + + + + + + + + installing pyopenssl"
-pip install pyopenssl
+pip install --upgrade pyopenssl
 echo "+ + + + + + + + + + installing cryptography"
-pip install cryptography
+pip install --upgrade cryptography
 echo "+ + + + + + + + + + installing dnspython"
-pip install dnspython
+pip install --upgrade dnspython
