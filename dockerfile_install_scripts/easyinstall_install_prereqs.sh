f0ad40f16b7bdf192a6621bc5493df2d503a064f
Ehud Kaldor <ehud@unfairfunction.org>
2018-02-19 14:11:16 -0800

    added install script using easy_install

---
+++ dockerfile_install_scripts/easyinstall_install_prereqs.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "+ + + + + + + + + + installing pastedeploy"
+easy_install-2.7 pastedeploy
+echo "+ + + + + + + + + + installing eventlet"
+easy_install-2.7 eventlet
+echo "+ + + + + + + + + + installing greenlet"
+easy_install-2.7 greenlet
+echo "+ + + + + + + + + + installing netifaces"
+easy_install-2.7 netifaces
+echo "+ + + + + + + + + + installing simplejson"
+easy_install-2.7 simplejson
+echo "+ + + + + + + + + + installing setuptools"
+easy_install-2.7 setuptools
+# echo "+ + + + + + + + + + installing distribute"
+# easy_install-2.7 distribute
+echo "+ + + + + + + + + + installing six"
+easy_install-2.7 six
+echo "+ + + + + + + + + + installing pyopenssl"
+easy_install-2.7 pyopenssl
+echo "+ + + + + + + + + + installing cryptography"
+easy_install-2.7 cryptography
+echo "+ + + + + + + + + + installing dnspython"
+easy_install-2.7 dnspython
